719529ce5e4f3e0941533fa7bace8e5a3877a5ff
Mikhail Vaysman <mikhail.vaysman@gmail.com>
2015-01-14 13:43:04 +0300

    finish SavingAccount test

--- src/main/java/SavingAccount.java
+++ src/main/java/SavingAccount.java
@@ -22,6 +22,8 @@ public class SavingAccount implements Account{
 
     @Override
     public void withdraw(float amount) {
-        balance -= amount;
+        if(balance >= amount) {
+            balance -= amount;
+        }
     }
 }
--- src/test/java/SavingAccountTest.java
+++ src/test/java/SavingAccountTest.java
@@ -7,34 +7,48 @@ import static org.hamcrest.Matchers.*;
 
 public class SavingAccountTest {
 
+    public static final float ONE_EURO = 1.0f;
+    public static final float TWO_EURO = 2.0f;
+    public static final float ZERO_EURO = 0.0f;
+    public static final float ERROR_DELTA = 0.00001f;
+
     @Test
     public void testDeposit() throws Exception {
-        Account account = new SavingAccount(0.0f);
+        Account account = new SavingAccount(ZERO_EURO);
 
-        account.deposit(1.0f);
-        assertThat((double) account.getBalance(), closeTo(1.0, 0.00001));
+        account.deposit(ONE_EURO);
+        assertThat((double) account.getBalance(), closeTo(ONE_EURO, ERROR_DELTA));
     }
 
     @Test
     public void testGetBalance() throws Exception {
-        Account account = new SavingAccount(1.0f);
+        Account account = new SavingAccount(ONE_EURO);
 
-        assertThat((double) account.getBalance(), closeTo(1.0, 0.00001));
+        assertThat((double) account.getBalance(), closeTo(ONE_EURO, ERROR_DELTA));
     }
 
     @Test
     public void testGetReport() throws Exception {
-        Account account = new SavingAccount(1.0f);
+        Account account = new SavingAccount(ONE_EURO);
 
         assertThat(account.getReport(), stringContainsInOrder(Arrays.asList("Saving", "1.0")));
     }
 
     @Test
     public void testWithdraw() throws Exception {
-        Account account = new SavingAccount(1.0f);
+        Account account = new SavingAccount(ONE_EURO);
 
-        account.withdraw(1.0f);
+        account.withdraw(ONE_EURO);
 
-        assertThat((double) account.getBalance(), is(closeTo(0.0, 0.00001)));
+        assertThat((double) account.getBalance(), is(closeTo(ZERO_EURO, ERROR_DELTA)));
+    }
+
+    @Test
+    public void testWithdrawWithAmountGreaterThanBalance() throws Exception {
+        Account account = new SavingAccount(ONE_EURO);
+
+        account.withdraw(TWO_EURO);
+
+        assertThat((double) account.getBalance(), is(closeTo(ONE_EURO, ERROR_DELTA)));
     }
 }
